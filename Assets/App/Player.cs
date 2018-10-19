760e0395aacb9d1bdfa1494894333d706d0ff6f1
Sonny Kurniawan <sonny.kurniawan.yap@gmail.com>
2018-10-20 00:22:13 +0900

    limit player movement inside the camera

--- Assets/App/Player.cs
+++ Assets/App/Player.cs
@@ -3,10 +3,22 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 	public float speed = 5;
+
+    Vector2 direction;
+
+    Bounds m_ShipBoundary;
+
 	// PlayerBulletプレハブ
 	public GameObject bullet;
-	// Use this for initialization
-	IEnumerator Start () {
+
+    private void Awake()
+    {
+        direction = new Vector2();
+        m_ShipBoundary = GetComponent<Renderer>().bounds;
+    }
+
+    // Use this for initialization
+    IEnumerator Start () {
 		while (true) {
             GameObject bullet = ObjectPooler.SharedInstance.GetPooledObject("Shot");
             bullet.transform.position = transform.position;
@@ -20,9 +32,26 @@ public class Player : MonoBehaviour {
 	void Update () {
 		float x = Input.GetAxisRaw ("Horizontal");
 		float y = Input.GetAxisRaw ("Vertical");
-		
-		Vector2 direction = new Vector2 (x, y).normalized;
-		
-		GetComponent<Rigidbody2D>().velocity = direction * speed;
-	}
+
+        if (!MoveAreaLimit(x, y))
+        {
+            direction = new Vector2(x, y).normalized;            
+        }
+        else
+        {
+            direction = Vector2.zero;
+        }
+        GetComponent<Rigidbody2D>().velocity = direction * speed;
+    }
+
+    public bool MoveAreaLimit(float xDir, float yDir)
+    {
+       
+        Vector2 min = Camera.main.WorldToViewportPoint(transform.position - m_ShipBoundary.min);
+        Vector2 max = Camera.main.WorldToViewportPoint(transform.position - m_ShipBoundary.max);
+
+        return (min.x < 0.05f && xDir < 0) || (min.y < 0.1f && yDir < 0) || (max.x > 0.95f && xDir > 0) || (max.y > 0.9f && yDir > 0) ? true : false;
+    }
+    
+
 }
