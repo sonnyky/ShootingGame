80f19e01fd3d982b5dfaa024472137244e69f0b7
Sonny Kurniawan <sonny.kurniawan.yap@gmail.com>
2019-12-25 00:38:59 +0900

    calibration flow

--- Assets/App/Scripts/Calibration/SensorCalibration.cs
+++ Assets/App/Scripts/Calibration/SensorCalibration.cs
@@ -10,26 +10,47 @@ public class SensorCalibration : MonoBehaviour
 
     bool m_IsCalibrating = false;
 
+    bool m_IsCalibratingPoint = false;
+
     // Start is called before the first frame update
     void Start()
     {
         m_Calibration = FindObjectOfType<Calibration>();
         m_SensorInput = FindObjectOfType<Tinker.Input>();
+
+        m_Calibration.OnCalibrationFinished += CalibrationFinished;
+        m_Calibration.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.C) && !m_IsCalibrating)
+        {
+            m_Calibration.gameObject.SetActive(true);
+            m_IsCalibrating = true;
+        }
+
         if(Tinker.Input.touchCount > 0)
         {
             Tinker.Touch touchInstance = Tinker.Input.GetTouch(0);
-            if(touchInstance.touchPhase == TouchPhase.Began)
+            if(touchInstance.touchPhase == TouchPhase.Began && m_IsCalibrating && !m_IsCalibratingPoint)
             {
-                m_IsCalibrating = true;
-            }else if(touchInstance.touchPhase == TouchPhase.Ended && m_IsCalibrating)
+                m_IsCalibratingPoint = true;
+                
+            }else if(touchInstance.touchPhase == TouchPhase.Ended && m_IsCalibrating && !m_IsCalibratingPoint)
             {
-
+                m_Calibration.SetSensorInputPoint(touchInstance.rawPosition);
+            }
+            else
+            {
+                // normal point display
             }
         }
+    }
+
+    void CalibrationFinished()
+    {
+        m_IsCalibrating = false;
     }
 }
